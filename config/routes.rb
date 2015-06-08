be2948786105cf95f483f73326b52f1bfa545681
mikhailkryukov <Rjhgjhfwbz1>
2015-06-08 13:39:59 -0700

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 SampleApp::Application.routes.draw do
+  get 'users/new'
+
+  get 'users/new'
+
+  get 'users/new'
+
   get "users/new"
 
-  root  'static_pages#home'
+
+
+  root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
